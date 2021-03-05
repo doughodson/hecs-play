920db41e86815a95a5dd37fdfcfe54c3a7378649
Doug Hodson <doug@sidechannel.net>
2021-03-05 08:25:29 -0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use hecs::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut world = World::new();
+    let a = world.spawn((123, true, "abc"));
+    let b = world.spawn((42, false));
+    // Systems can be simple for loops
+    for (_id, (number, &flag)) in world.query_mut::<(&mut i32, &bool)>() {
+      if flag { *number *= 2; }
+    }
+    // Random access is simple and safe
+    assert_eq!(*world.get::<i32>(a).unwrap(), 246);
+    assert_eq!(*world.get::<i32>(b).unwrap(), 42);
 }
